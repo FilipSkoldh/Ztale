3831fb397b07a2ccb9e1c7f570a275c16695f7d1
Filip Sköld <22skofil@soltorgsgymnasiet.se>
2023-11-27 12:07:07 +0100

    Started writing C#

--- Assets/Scripts/Character script.cs
+++ Assets/Scripts/Character script.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class Characterscript : MonoBehaviour
 {
+    public InputActionAsset actions;
+    private InputAction moveaction;
+
+
+
+
+    private void Awake()
+    {
+        moveaction = actions.FindActionMap("char").FindAction("move");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,6 @@ public class Characterscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveVector = moveaction.ReadValue<Vector2>();
     }
 }
