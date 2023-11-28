23262218edf2430fcf1c9d76996c0f5aca1f3b72
22skofil <22skofil@303-H52>
2023-11-28 15:07:48 +0100

    fixed crouch, sprint, moving animation and more?

--- Assets/Scripts/Character script.cs
+++ Assets/Scripts/Character script.cs
@@ -7,13 +7,18 @@ public class Characterscript : MonoBehaviour
 {
     ///Declaring shit
     public float speed = 1;
-    public float sprint = 0;
     public float stamina = 10;
     public float staminacooldown = 1;
-    public InputActionAsset actions;
+    private bool sprint;
+    private bool crouch;
+    private bool sprintLF = false;
+    private Animator anim;
     private InputAction moveaction;
     private InputAction sprintaction;
+    private InputAction crouchaction;
     private Rigidbody2D rb;
+    private Transform trans;
+    public InputActionAsset actions;
 
     void OnEnable()
     {
@@ -24,7 +29,10 @@ public class Characterscript : MonoBehaviour
     {
         moveaction = actions.FindActionMap("char").FindAction("move");
         sprintaction = actions.FindActionMap("char").FindAction("sprint");
+        crouchaction = actions.FindActionMap("char").FindAction("crouch");
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        trans = GetComponent<Transform>();
     }
 
     // Start is called before the first frame update
@@ -36,26 +44,51 @@ public class Characterscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        sprint = sprintaction.IsPressed();
+        crouch = crouchaction.IsPressed();
+
+        trans.localScale = new Vector3(1, 1, 1);
+        speed = 1;
+        
         Vector2 movevector = moveaction.ReadValue<Vector2>();
-        sprint = sprintaction.ReadValue<float>();
-        if (sprint > 0 && stamina > 0 && staminacooldown <= 0)
+        if (movevector != Vector2.zero)
         {
-            speed = 2;
-            stamina = stamina - Time.deltaTime * 1;
+            anim.SetBool("moving", true);
+            anim.SetFloat("x", movevector.x);
+            anim.SetFloat("y", movevector.y);
         }
         else
         {
-            speed = 1;
+            anim.SetBool("moving", false);
+        }
+        if (crouch)
+        {
+            trans.localScale = new Vector3(1, 0.4f, 1);
+            speed = 0.5f;
+        }
+
+        if (sprint && !crouch && stamina > 0 && movevector != Vector2.zero && (staminacooldown < 0.1 || sprintLF))
+        {
+            speed = 2;
+            stamina = stamina - Time.deltaTime * 1;
+            staminacooldown = 1;
+            sprintLF = true;
+            anim.speed = speed;
+        }
+        else
+        {
+            anim.speed = speed;
             if (staminacooldown > 0)
             {
                 staminacooldown = staminacooldown - Time.deltaTime * 1;
+                sprintLF = false;
             }
             else if (stamina < 10)
             {
                 stamina = stamina + Time.deltaTime * 1.5f;
             }
         }
-        Debug.Log(stamina);
+        Debug.Log(speed);   
         rb.velocity = movevector * speed;
     }
 }
