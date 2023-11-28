edc21b9de4734a1f4fc7eceabecf3371629b92c2
Filip Sköld <22skofil@soltorgsgymnasiet.se>
2023-11-28 10:07:32 +0100

    movement script started and we can move

--- Assets/Scripts/Character script.cs
+++ Assets/Scripts/Character script.cs
@@ -5,26 +5,48 @@ using UnityEngine;
 
 public class Characterscript : MonoBehaviour
 {
+    ///Declaring shit
+    public float speed = 1;
+    public float sprint = 0;
+    public float stamina = ;
     public InputActionAsset actions;
     private InputAction moveaction;
+    private InputAction sprintaction;
+    private Rigidbody2D rb;
 
-
-
+    void OnEnable()
+    {
+        actions.FindActionMap("char").Enable();
+    }
 
     private void Awake()
     {
         moveaction = actions.FindActionMap("char").FindAction("move");
+        sprintaction = actions.FindActionMap("char").FindAction("sprint");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVector = moveaction.ReadValue<Vector2>();
+        Vector2 movevector = moveaction.ReadValue<Vector2>();
+        sprint = sprintaction.ReadValue<float>();
+        if (sprint > 0)
+        {
+            speed = 2;
+            stamina = stamina - Time.deltaTime * 1;
+        }
+        else
+        {
+            speed = 1;
+        }
+        Debug.Log(movevector);
+        rb.velocity = movevector * speed;
     }
 }
