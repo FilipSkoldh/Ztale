9ecb695cfa490f500b738acf7fe260a132d245e4
22skofil <22skofil@soltorgsgymnasiet.se>
2024-03-05 14:19:50 +0100

    Can open inv, see items added

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,6 +1,7 @@
 using QuantumTek.QuantumInventory;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -10,15 +11,40 @@ public class Inventory : MonoBehaviour
     [SerializeField] private InputActionProperty openInv;
     [SerializeField] private GameObject invGameobj;
     [SerializeField] private EventSystem eventSystem;
+    [SerializeField] private List<GameObject> invList;
     public QI_Inventory inventory;
     public QI_ItemDatabase itemDatabase;
-    private bool pressedlastframe;
+    private Dictionary<string, QI_ItemData> items = new Dictionary<string, QI_ItemData>();
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        items.Add("Shotgun", itemDatabase.GetItem("Shotgun"));
+        items.Add("Bandages", itemDatabase.GetItem("Bandages"));
+        inventory.AddItem(items["Shotgun"], 1);
+        inventory.AddItem(items["Bandages"], 5);
     }
+    private void invRefresh()
+    {
+        for (int i = 0; i < inventory.Stacks.Count; i++)
+        {
+            GameObject itemRefresh = invList[i];
+            itemRefresh.SetActive(true);
+            
+            if (inventory.GetStock(inventory.Stacks[i].Item.Name) != itemDatabase.GetItem(inventory.Stacks[i].Item.Name).MaxStack)
+            {
+                itemRefresh.GetComponent<TextMeshProUGUI>().text = $"- {inventory.Stacks[i].Item.Name} x{inventory.GetStock(inventory.Stacks[i].Item.Name)}";
+            }
+            else
+            {
+                itemRefresh.GetComponent<TextMeshProUGUI>().text = $"- {inventory.Stacks[i].Item.Name}";
+            }
 
+
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -34,11 +60,12 @@ public class Inventory : MonoBehaviour
                 invGameobj.SetActive(true);
                 if (invGameobj.transform.GetChild(12).gameObject.activeSelf)
                   eventSystem.SetSelectedGameObject(invGameobj.transform.GetChild(12).GetChild(0).gameObject);
-
+                invRefresh();
             }
         }
         if (invGameobj.activeSelf)
         {
+
 
         }
     }
