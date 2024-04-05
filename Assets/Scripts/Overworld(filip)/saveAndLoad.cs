a8d9429ebfded2b2b663aeef415969a204f6a274
Filip Sköld <filip.skoldh@gmail.com>
2024-04-06 01:24:01 +0200

    i fucking hate visaul on my pc

--- Assets/Scripts/Overworld(filip)/saveAndLoad.cs
+++ Assets/Scripts/Overworld(filip)/saveAndLoad.cs
@@ -6,6 +6,18 @@ using UnityEngine;
 
 public class saveAndLoad : MonoBehaviour
 {
+    public QI_Inventory pInventory;
+    public QI_Inventory chest1;
+    public QI_Inventory chest2;
+    public QI_Inventory chest3;
+    public QI_Inventory chest4;
+    public QI_Inventory chest5;
+    public QI_Inventory chest6;
+    public QI_Inventory chest7;
+    public QI_Inventory chest8;
+    public QI_Inventory chest9;
+    public QI_Inventory chest10;
+
     // Start is called before the first frame update
     void Start()
     {
