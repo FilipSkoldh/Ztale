1fd98856a1b11baaf8b6229660145d5df5326d9b
22skofil <22skofil@soltorgsgymnasiet.se>
2024-04-23 10:09:19 +0200

    Comments s1e4

--- Assets/Scripts/Overworld(filip)/Chest/interactWithChest.cs
+++ Assets/Scripts/Overworld(filip)/Chest/interactWithChest.cs
@@ -7,54 +7,72 @@ using UnityEngine.InputSystem;
 
 public class InteractWithChest : MonoBehaviour
 {
-    [SerializeField] private InteractWithInventory openInventory;
-    [SerializeField] private GameObject chestGameobj;
-    [SerializeField] private EventSystem eventSystem;
-    [SerializeField] private List<GameObject> invList;
-    [SerializeField] private SaveAndLoad saveAndLoad;
+    //Declares variables
+
+    //All inputs needed
     [SerializeField] private InputActionProperty inv;
     [SerializeField] private InputActionProperty close;
-    [SerializeField] private QI_Chest playerChest;
+
+    //Chest GUI
+    [SerializeField] private GameObject chestGameobj;
+
+    //Chests inventory slots and the EventSystem
+    [SerializeField] private List<GameObject> invList;
+    [SerializeField] private EventSystem eventSystem;
+
+    //Chests inventory and chest moving script
     private QI_Inventory inventory;
+    QI_Chest chestVendor;
+
+    //the database and dictionary with all item info
     public QI_ItemDatabase itemDatabase;
     private Dictionary<string, QI_ItemData> items = new();
-    QI_Chest chestVendor;
-    private void Start()
+
+    //Other scripts
+    [SerializeField] private InteractWithInventory openInventory;
+    [SerializeField] private SaveAndLoad saveAndLoad;
+    [SerializeField] private QI_Chest playerChestVendor;
+
+    private void Awake()
     {
         items = itemDatabase.Getdictionary();
+        inventory.AddItem(items["Bandage"], 10);
+        inventory.AddItem(items["Pistol"], 1);        
     }
 
     public void OpenChest(int inventoryNumber)
     {
+        chestVendor = saveAndLoad.transferChests[inventoryNumber+1];
         inventory = saveAndLoad.chests[inventoryNumber];
-        inventory.AddItem(items["Bandage"], 10);
-        inventory.AddItem(items["Pistol"], 1);
         chestGameobj.SetActive(true);
         openInventory.OpenOrCloseInventory();
-        chestVendor = saveAndLoad.transferChests[inventoryNumber+1];
 
         RefreshChest();
     }
 
     public void RefreshChest()
     {
+        //Resets all textboxes in the chest GUI
         foreach (var item in invList)
         {
             item.GetComponent<TextMeshProUGUI>().text = "-";
         }
+        //Sorts the chests inventory in alphabetical order
+        inventory.Stacks.Sort((p1, p2) => { return string.Compare(p1.Item.name, p2.Item.name); });
 
+        //Reads the chests inventory and puts it in the corresponding positions in the inventory GUI
+        //for every stack in the inventory
         for (int i = 0; i < inventory.Stacks.Count; i++)
         {
-            GameObject itemRefresh = invList[i];
-            itemRefresh.SetActive(true);
-
-            if (inventory.GetStock(inventory.Stacks[i].Item.Name) != itemDatabase.GetItem(inventory.Stacks[i].Item.Name).MaxStack)
+            //If the item is stackable write out the name of the item and the amount
+            if (itemDatabase.GetItem(inventory.Stacks[i].Item.Name).MaxStack != 1)
             {
-                itemRefresh.GetComponent<TextMeshProUGUI>().text = $"- {inventory.Stacks[i].Item.Name} x{inventory.GetStock(inventory.Stacks[i].Item.Name)}";
+                invList[i].GetComponent<TextMeshProUGUI>().text = $"- {inventory.Stacks[i].Item.Name} x{inventory.Stacks[i].Amount}";
             }
+            //Else write out only the name
             else
             {
-                itemRefresh.GetComponent<TextMeshProUGUI>().text = $"- {inventory.Stacks[i].Item.Name}";
+                invList[i].GetComponent<TextMeshProUGUI>().text = $"- {inventory.Stacks[i].Item.Name}";
             }
         }
     }
@@ -63,7 +81,7 @@ public class InteractWithChest : MonoBehaviour
     {
         if (inventory.Stacks.Count > button)
         {
-            QI_Chest.Transaction(playerChest, chestVendor, inventory.Stacks[button].Item, 1);
+            QI_Chest.Transaction(playerChestVendor, chestVendor, inventory.Stacks[button].Item, 1);
             RefreshChest();
             openInventory.RefreshInventory();
         }
